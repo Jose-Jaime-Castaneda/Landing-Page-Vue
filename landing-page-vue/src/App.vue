e76dcd5d7fa07d5a887ba19724514819cd133760
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-07-05 14:59:24 -0600

    Componentes del navbar

--- landing-page-vue/src/App.vue
+++ landing-page-vue/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <nav class="navbar"></nav>
+    <nav class="navbar">
+      <div class="empresa">
+        <a href="" class="logo">Mi compañia</a>
+      </div>
+      <div class="menu-navbar">
+        <ul class="navbar-ul">
+          <i><a href="">Inicio</a></i>
+          <i><a href="">Acerca de</a></i>
+          <i><a href="">PYR</a></i>
+          <i><a href="">Contacto</a></i>
+        </ul>
+      </div>
+      <button class="navbar-toggle">azs</button>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -13,4 +26,9 @@ export default {
 
 <style>
 @import "./Style/Navbar.css";
+
+* {
+  margin: 0;
+  padding: 0;
+}
 </style>
