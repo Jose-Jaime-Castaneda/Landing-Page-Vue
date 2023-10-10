6e28f97e9b25b085b49a1e9ce1bb8e0736b35b71
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-10-09 19:25:51 -0600

    Vista P&R lista

--- landing-page-vue/src/components/Faq.vue
+++ landing-page-vue/src/components/Faq.vue
@@ -1,5 +1,84 @@
 <template>
-  <div class="faq">FAQ</div>
+  <section>
+    <div class="faq">
+      <article class="encabezado">
+        <h2>P&R</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+          distinctio molestias saepe quibusdam enim iste assumenda ea incidunt
+          tempore sequi odit eius, ducimus nostrum quo corrupti autem,
+          doloremque repellat maiores.
+        </p>
+      </article>
+    </div>
+    <div class="opinion-section">
+      <section class="opinions">
+        <p class="opinion">
+          "Pregunta frecuente 1" <br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 2"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 3"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 4"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 5"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 6"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 7"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 8"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+        <p class="opinion">
+          "Pregunta frecuente 9"<br /><br />
+          <span class="answer"
+            >Respuesta de la empresa al cliente para la pregunta de
+            arriba...</span
+          >
+        </p>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,3 +88,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../Style/Faq.css";
+</style>
