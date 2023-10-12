684772a81e06f005f3dfaaa366315b2219973564
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-10-11 19:26:17 -0600

    Ajustes para Footer

--- landing-page-vue/src/components/Footer.vue
+++ landing-page-vue/src/components/Footer.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="PageFooter">
-        <footer class="footer">
-            This is teh footer of the page.
-        </footer>
-    </div>
+  <div class="PageFooter">
+    <footer class="footer">
+      <div class="texto">
+        <p class="siguenos">Siguenos en nuestras redes sociales.</p>
+      </div>
+      <div class="redes">
+        <div class="facebook">
+          <img src="../../resources/facebook-square-brands.png" alt="" />
+        </div>
+        <div class="twitter">
+          <img src="../../resources/twitter-square-brands.png" alt="" />
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -14,4 +24,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "../Style/Footer.css";
 </style>
