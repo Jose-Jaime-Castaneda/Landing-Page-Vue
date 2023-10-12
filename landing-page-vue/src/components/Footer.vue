a9eecafab983726729299e07c3fca81cbfb46bc0
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-10-11 18:56:16 -0600

    Se crea Footer

---
+++ landing-page-vue/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="PageFooter">
+        <footer class="footer">
+            This is teh footer of the page.
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
